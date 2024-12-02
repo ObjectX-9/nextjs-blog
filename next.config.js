240b3463b426e17aba2206d99e6d7ded8dc9a669
zhuling822 <ling.zhu@js.design>
2024-12-02 15:44:52 +0800

    fix: error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'iad.microlink.io'],
+    domains: ['images.unsplash.com', 'iad.microlink.io', 'avatars.githubusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
