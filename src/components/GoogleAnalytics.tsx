bf3204e31d93d45d111d447933ef95122da4eb1a
zhuling822 <ling.zhu@js.design>
2025-02-21 12:08:23 +0800

    fix: bug

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,11 +5,13 @@ import Script from 'next/script';
 
 export default function GoogleAnalytics() {
   const { site } = useSiteStore();
+  
+  if (!site?.googleAnalyticsId) return null;
 
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${site?.googleAnalyticsId}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${site.googleAnalyticsId}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -18,7 +20,7 @@ export default function GoogleAnalytics() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${site?.googleAnalyticsId}');
+          gtag('config', '${site.googleAnalyticsId}');
         `}
       </Script>
     </>
