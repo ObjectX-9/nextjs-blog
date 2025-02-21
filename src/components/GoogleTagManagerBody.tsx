15a3ace3de538005b0196e6782d858fec3feb46d
zhuling822 <ling.zhu@js.design>
2025-02-21 14:29:15 +0800

    feat: GTM

--- src/components/GoogleTagManagerBody.tsx
+++ src/components/GoogleTagManagerBody.tsx
@@ -8,13 +8,17 @@ export default function GoogleTagManagerBody() {
   if (!site?.googleTagManagerId) return null;
 
   return (
-    <noscript>
-      <iframe 
-        src={`https://www.googletagmanager.com/ns.html?id=${site.googleTagManagerId}`}
-        height="0" 
-        width="0" 
-        style={{ display: 'none', visibility: 'hidden' }}
-      />
-    </noscript>
+    <noscript
+      dangerouslySetInnerHTML={{
+        __html: `
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=${site.googleTagManagerId}"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        `
+      }}
+    />
   );
 }
