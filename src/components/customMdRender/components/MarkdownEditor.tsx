786aba55297a41520b40dfc1dcff5d66efd966b8
zhuling904 <ling.zhu.904@gmail.com>
2025-08-21 22:37:55 +0800

    fix: 去掉主题颜色

--- src/components/customMdRender/components/MarkdownEditor.tsx
+++ src/components/customMdRender/components/MarkdownEditor.tsx
@@ -4,7 +4,6 @@ import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
-import './MarkdownThemes.css';
 
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 import Prism from 'prismjs';
--- src/components/customMdRender/core/MarkdownRenderer.tsx
+++ src/components/customMdRender/core/MarkdownRenderer.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useRef } from 'react';
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
-import '../components/MarkdownThemes.css';
 import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 // 导入常用语言支持
